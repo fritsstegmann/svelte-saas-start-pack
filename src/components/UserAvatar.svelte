3697dab0e60a8da652682dc846c3c9ace94fdb42
frits-metalogix <fstegmann@gmail.com>
2024-09-14 18:01:59 +0200

    feat: user avatar upgrade

--- src/components/UserAvatar.svelte
+++ src/components/UserAvatar.svelte
@@ -17,7 +17,7 @@
         viewBox="0 0 24 24"
         stroke-width="1"
         stroke="currentColor"
-        class="size-7 rounded-full border border-black/5 text-gray-400 shadow-inner shadow-white">
+        class="size-7 rounded-full text-gray-400">
         <path
             stroke-linecap="round"
             stroke-linejoin="round"
