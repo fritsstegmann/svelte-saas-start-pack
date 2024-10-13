53cae83a8c0c6f193da201ac9e2a9eb65770bce2
Frits Stegmann <fstegmann@gmail.com>
2024-10-13 09:44:28 +0200

    feat: some updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,23 @@ import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'selector',
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: colors.violet,
-				secondary: colors.purple,
-				accent: colors.fuchsia,
-				gray: colors.stone
-			}
-		}
-	},
-	plugins: []
+    darkMode: 'selector',
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        fontFamily: {
+            inter: ['Inter'],
+            sansTest: ['Red Hat Display', 'Noto Sans Display', 'Inter Tight', 'Arial', 'Open Sans'],
+            // sans: ['Open Sans'],
+            serif: ['Georgia'],
+        },
+        extend: {
+            colors: {
+                primary: colors.violet,
+                secondary: colors.purple,
+                accent: colors.fuchsia,
+                gray: colors.stone,
+            },
+        },
+    },
+    plugins: [],
 };
